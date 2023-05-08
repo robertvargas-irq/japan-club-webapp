437688c964a263aa164a5ea935d31bec2b562ef3
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-08 12:18:31 -0600

    added navbar based on bootstrap's docs

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Japan Culture Club
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" href="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/scan">
+                Scan
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/generate">
+                Generate
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
