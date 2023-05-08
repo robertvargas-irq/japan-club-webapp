0baa0776258eed9ee0bf1d8869a12eae45849f70
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-08 11:28:02 -0600

    switched to bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,15 +15,15 @@ export default function Home() {
         <h1 className="text-center">QR Make & Scan</h1>
 
         <div className="grid grid-cols-2">
-          <a href="/scan">
+          <Link href="/scan">
             <h2>QR Scan &rarr;</h2>
             <p>Use your camera to scan!</p>
-          </a>
+          </Link>
 
-          <a href="/generate">
+          <Link href="/generate">
             <h2>QR Generator &rarr;</h2>
             <p>Generate a new code!</p>
-          </a>
+          </Link>
         </div>
       </main>
     </div>
